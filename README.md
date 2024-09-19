d49ca1fad4b987f6f0497a952ca3562905b7c6c0
unknown <umayerpro175@gmail.com>
2024-09-19 16:23:22 +0600

    update

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 git commit -m "first commit"
 git branch -M main
-git remote add origin https://github.com/UmayerCoding1/mobile-store-server.git
+git remote add origin https://github.com/UmayerCoding1/mobile-store-d-server.git
 git push -u origin main
--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const app = express();
 const port = process.env.PORT || 5000;
@@ -38,6 +38,13 @@ async function run() {
         const cursor = mobileCollections.find()
         const result =await cursor.toArray();
         res.send(result);
+    })
+
+    app.get('/mobiles/:id', async(req,res) => {
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)};
+      const result = await mobileCollections.findOne(query);
+      res.send(result);
     })
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
